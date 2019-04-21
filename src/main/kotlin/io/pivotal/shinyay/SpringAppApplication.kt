8ffbabf07b248f77385c2262a415f0d0569af970
shinyay <shinya.com@gmail.com>
2019-04-21 22:18:44 +0900

    Add kdoc sample

--- src/main/kotlin/io/pivotal/shinyay/SpringAppApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringAppApplication.kt
@@ -3,9 +3,16 @@ package io.pivotal.shinyay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Entry Class
+ * @author shinyay
+ */
 @SpringBootApplication
 class SpringAppApplication
 
+/**
+ * Entry point
+ */
 fun main(args: Array<String>) {
 	runApplication<SpringAppApplication>(*args)
 }
